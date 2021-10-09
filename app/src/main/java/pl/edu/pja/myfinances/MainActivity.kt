4baa114fcaf9ac4ef080e4095e6a83a346247528
Jan Kwasowski <61729697+s19191@users.noreply.github.com>
2021-10-10 00:42:54 +0200

    Dodane przyciski do wylogowywania się oraz sprawdzanie czy użytkownik jest zalogowany

--- app/src/main/java/pl/edu/pja/myfinances/MainActivity.kt
+++ app/src/main/java/pl/edu/pja/myfinances/MainActivity.kt
@@ -1,14 +1,29 @@
 package pl.edu.pja.myfinances
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.google.firebase.auth.FirebaseAuth
 import pl.edu.pja.myfinances.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        auth = FirebaseAuth.getInstance()
+
+        if (auth.currentUser == null) {
+            startActivity(Intent(this, LogInActivity::class.java))
+        }
+    }
+
+    fun signOut(view: View) {
+        auth.signOut()
+        startActivity(Intent(this, LogInActivity::class.java))
     }
 }
